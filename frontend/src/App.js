b30287c79eed5e5eff8788d22e8a45a2b3a82f2a
Nabiha <islamnabiha10@gmail.com>
2026-09-18 13:29:14 -0700

    added create resume component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header'
 import Welcome from './components/Welcome'
+import Create from './components/Create';
 
 const App = () => {
   return (
     <div className="App-container">
       <Header title = '"Comparison is the thief of joy"'></Header>
       <Welcome message = 'Welcome, user'></Welcome>
+      <Create></Create>
     </div>
   );
 }
---
+++ frontend/src/components/Create.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+const Create = () => {
+    return (
+        <div className = "create-card">
+            <input className = "plus-button" type = "submit" value = "" ></input>
+            <p>Create your first resume!</p>
+        </div>
+    )
+}
+
+export default Create
